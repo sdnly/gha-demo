234dfd65f83e4625ed0b8dc0e2cb6e51ce90baaa
sdnly <nesentic@gmail.com>
2023-11-27 22:57:33 +0100

    init

---
+++ src/main/java/com/example/ghademo/GhaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhaDemoApplication.class, args);
+	}
+
+}
